c1b4317d4b58cac3076f28c0c7699e3b703cdca5
Sapfuldog <sapfuldog@gmail.com>
2024-07-16 18:59:24 +0300

    1

--- app.py
+++ app.py
@@ -2,33 +2,26 @@ from dash import Dash, html
 import altair as alt
 import dash_vega_components as dvc
 import plotly.express as px
+import pandas as pd
 
-df = px.data.tips()
-chart = (
-    alt.Chart(df)
-    .mark_circle(size=60)
-    .encode(
-        x="tip",
-        y="total_bill",
-        color=alt.Color("day").scale(domain=["Thur", "Fri", "Sat", "Sun"]),
-        tooltip=["day", "tip", "total_bill"],
-    )
-    .interactive()
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='my dashboard app', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
 )
 
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
 
-app = Dash()
-app.layout = html.Div(
-    [
-        html.H1("Vega-Altair Chart in a Dash App"),
-        dvc.Vega(
-            id="altair-chart",
-            opt={"renderer": "svg", "actions": False},
-            spec=chart.to_dict(),
-        ),
-    ]
-)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8050)
